88c7ff34c2c05debf9a5901d4f4c6bdb92d24369
Gideonkipyegon <gideonkipyegon837@gmail.com>
2024-02-29 15:55:31 +0300

    first commit

---
+++ datatypes.py
@@ -0,0 +1,45 @@
+#learning python datatypes
+# Integer
+x = 5
+print(x)
+print(type(x))  # Output: <class 'int'>
+
+# Float
+y = 3.14
+print(y)
+print(type(y))  # Output: <class 'float'>
+
+# String
+name = "John"
+print(name)
+print(type(name))  # Output: <class 'str'>
+
+# Boolean
+is_valid = True
+print(is_valid)
+print(type(is_valid))  # Output: <class 'bool'>
+
+# List
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+print(type(numbers))  # Output: <class 'list'>
+
+# Tuple
+coordinates = (10, 20)
+print(coordinates)
+print(type(coordinates))  # Output: <class 'tuple'>
+
+# Dictionary
+person = {'name': 'John', 'age': 30}
+print(person)
+print(type(person))  # Output: <class 'dict'>
+
+# Set
+unique_numbers = {1, 2, 3, 4, 5}
+print(unique_numbers)
+print(type(unique_numbers))  # Output: <class 'set'>
+
+# NoneType
+empty_value = None
+print(empty_value)
+print(type(empty_value))  # Output: <class 'NoneType'>
